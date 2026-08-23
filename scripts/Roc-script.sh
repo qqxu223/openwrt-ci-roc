2f56d7993ec938c5cff0c2509b9311a29cd6f070
Roc Lai <laipeng668@qq.com>
2026-08-23 08:03:22 +0800

    Personal Tailor

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -14,7 +14,7 @@ sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.r
                 ? boardinfo.release.description + ' / '\n \
                 : '') + (luciversion || '') + ' / ',\n \
             E('a', {\n \
-                href: 'https://github.com/laipeng668/openwrt-ci-roc/releases',\n \
+                href: 'https://github.com/qqxu223/openwrt-ci-roc/releases',\n \
                 target: '_blank',\n \
                 rel: 'noopener noreferrer'\n \
                 }, [ 'Built by Roc $(date "+%Y-%m-%d %H:%M:%S")' ])\n \
